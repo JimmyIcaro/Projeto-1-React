0501cb2693ed17ed332ceef1cb15f717cc613c4f
Jimmy Ícaro <159497295+JimmyIcaro@users.noreply.github.com>
2024-10-16 16:03:28 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+export default function App() {
+  return (
+    <div className="App">
+      <img src='../public/react.png'></img>
+      
+      <h1>React</h1>
+      
+      <p>A biblioteca para interfaces de usuário web e nativas</p>
+      <button>Aprenda React</button> <button>Referências API</button>
+      <hr></hr>
+      <h1>Crie interfaces de usuáro de componentes</h1>
+      <p>React permite que você construa interfaces a partir de pedaços individuais chamados componentes</p>
+      <hr></hr>
+      <h1>Escreva componentes com código e marcação</h1>
+      <p>Componentes React são funções JaavaScripit. A sintaxe de marcação é chamada de JSX. É uma extensão de sintaxe JavaScript popularizaada pelo React</p>
+      <hr></hr>
+      <h1>Próximos passos</h1>
+      
+      <ul>
+        <li>Uso de dados dinâmicos n JSX</li>
+        <li>Criação de novos componentes</li>
+        <li>Estilização de componentes </li>
+        <li>Reulilizalição de componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos do JavaScript</li>
+      </ul>
+    
+    </div>
+  )
+}
